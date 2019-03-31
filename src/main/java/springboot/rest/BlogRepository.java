2d7ef404a2731e451f400ef7cf915dd667605c9f
tatiana <sorokina.tatiana.ev@gmail.com>
2019-03-31 21:35:25 +0200

    upgrade project

---
+++ src/main/java/springboot/rest/BlogRepository.java
@@ -0,0 +1,14 @@
+package springboot.rest;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BlogRepository extends JpaRepository<Blog, Integer> {
+
+    // custom query to search to blog post by title or content
+    List<Blog> findByTitleContainingOrContentContaining(String text, String textAgain);
+
+}
